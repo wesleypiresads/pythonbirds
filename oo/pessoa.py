8fd14fda1041820987f44cd4dd99922f67c15777
Wesley Pires <wesleypiresads@gmail.com>
2020-04-05 22:24:34 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,9 @@
 class Pessoa:
 
-    def __init__(self, nome=None, idade=38):
+    def __init__(self, *filhos, nome=None, idade=38):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return 'Criando Classe Objetos e Atributos'
@@ -10,7 +11,10 @@ class Pessoa:
 
 if __name__ == "__main__":
     
-    p = Pessoa('Wesley Pires')
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+    wesley = Pessoa(nome='Wesley')
+    pires  = Pessoa(wesley, nome='Pires')
+    print(pires.cumprimentar())
+    print(pires.nome)
+    print(pires.idade)
+    for filho in pires.filhos:
+        print(filho.nome)
